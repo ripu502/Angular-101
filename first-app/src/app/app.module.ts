f27f97b91d5a30c3a26a6dc96f3d38a2f9a23734
ripu502 <ripu502@gmail.com>
2021-07-20 20:12:37 +0530

    static component data

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppCartComponent } from './components/app-cart/app-cart.component';
+import { CartContainerComponent } from './components/cart-container/cart-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppCartComponent,
+    CartContainerComponent
   ],
   imports: [
     BrowserModule,
