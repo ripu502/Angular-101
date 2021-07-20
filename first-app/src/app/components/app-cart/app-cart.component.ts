1f78b9b94a1d109858bd4cdd010269228c43907a
ripu502 <ripu502@gmail.com>
2021-07-20 20:17:15 +0530

    added states for the component

--- first-app/src/app/components/app-cart/app-cart.component.ts
+++ first-app/src/app/components/app-cart/app-cart.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppCartComponent implements OnInit {
 
+  title: string = "Card title";
+  description: string = "Some quick example text to build on the card title and make up the bulk of the card's content. 1";
+  
   constructor() { }
 
   ngOnInit(): void {
