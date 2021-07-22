21f75ddbe23dadfb4624e26b32ebe668f827b4c3
ripu502 <ripu502@gmail.com>
2021-07-22 22:34:07 +0530

    added the dynamic component in the form

--- first-app/src/app/components/name-editor/name-editor.component.ts
+++ first-app/src/app/components/name-editor/name-editor.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-name-editor',
@@ -13,18 +16,26 @@ export class NameEditorComponent implements OnInit {
   //   lastName: new FormControl(''),
   // });
 
+  // very helpful used the docs
+  // https://angular.io/guide/reactive-forms
+  // in this covers only one validator any be more will try to cover later
+  // Cover making dynamic form with form Array also used the form builder for less code
+
   profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+    firstName: new FormControl('',  Validators.required),
+    lastName: new FormControl('', [Validators.required, Validators.email]),
     address: new FormGroup({
       street: new FormControl(''),
       city: new FormControl(''),
       state: new FormControl(''),
       zip: new FormControl('')
-    })
+    }),
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
   });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
@@ -34,4 +45,11 @@ export class NameEditorComponent implements OnInit {
     console.warn(this.profileForm.value);
   }
 
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+  
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
+  }
 }
